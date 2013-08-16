def66a558fc0f495f681083c03acf2801c940555
pickupausername <ssw255@qq.com>
2013-08-15 18:26:05 -0700

    final

---
+++ final_project_team_[9]/website/js/rankscript.js
@@ -0,0 +1,320 @@
+
+	$(document).ready(function(){
+		$("#cee1").click(function(){
+		$("#cee1").css('background-color','white');
+		$("#cee1").css('color','black');
+		$("#cee").css('display','inline');
+
+		$("#bioe1").css('background-color','black');
+		$("#bioe1").css('color','white')
+		$("#bioe").css('display','none');
+		$("#ee1").css('background-color','black');
+		$("#ee1").css('color','white')
+		$("#ee").css('display','none');
+		$("#cs1").css('background-color','black');
+		$("#cs1").css('color','white')
+		$("#cs").css('display','none');
+		$("#ieor1").css('background-color','black');
+		$("#ieor1").css('color','white')
+		$("#ieor").css('display','none');
+		$("#mse1").css('background-color','black');
+		$("#mse1").css('color','white')
+		$("#mse").css('display','none');
+		$("#me1").css('background-color','black');
+		$("#me1").css('color','white')
+		$("#me").css('display','none');
+		$("#ne1").css('background-color','black');
+		$("#ne1").css('color','white')
+		$("#ne").css('display','none');
+	});
+	});
+
+
+	$(document).ready(function(){
+		$("#bioe1").click(function(){
+		$("#bioe1").css('background-color','white');
+		$("#bioe1").css('color','black');
+		$("#bioe").css('display','inline');
+
+		$("#cee1").css('background-color','black');
+		$("#cee1").css('color','white')
+		$("#cee").css('display','none');
+		$("#ee1").css('background-color','black');
+		$("#ee1").css('color','white')
+		$("#ee").css('display','none');
+		$("#cs1").css('background-color','black');
+		$("#cs1").css('color','white')
+		$("#cs").css('display','none');
+		$("#ieor1").css('background-color','black');
+		$("#ieor1").css('color','white')
+		$("#ieor").css('display','none');
+		$("#mse1").css('background-color','black');
+		$("#mse1").css('color','white')
+		$("#mse").css('display','none');
+		$("#me1").css('background-color','black');
+		$("#me1").css('color','white')
+		$("#me").css('display','none');
+		$("#ne1").css('background-color','black');
+		$("#ne1").css('color','white')
+		$("#ne").css('display','none');
+	});
+	});
+	
+	$(document).ready(function(){
+		$("#ee1").click(function(){
+		$("#ee1").css('background-color','white');
+		$("#ee1").css('color','black');
+		$("#ee").css('display','inline');
+
+		$("#bioe1").css('background-color','black');
+		$("#bioe1").css('color','white')
+		$("#bioe").css('display','none');
+		$("#cee1").css('background-color','black');
+		$("#cee1").css('color','white')
+		$("#cee").css('display','none');
+		$("#cs1").css('background-color','black');
+		$("#cs1").css('color','white')
+		$("#cs").css('display','none');
+		$("#ieor1").css('background-color','black');
+		$("#ieor1").css('color','white')
+		$("#ieor").css('display','none');
+		$("#mse1").css('background-color','black');
+		$("#mse1").css('color','white')
+		$("#mse").css('display','none');
+		$("#me1").css('background-color','black');
+		$("#me1").css('color','white')
+		$("#me").css('display','none');
+		$("#ne1").css('background-color','black');
+		$("#ne1").css('color','white')
+		$("#ne").css('display','none');
+	});
+	});
+	
+	$(document).ready(function(){
+		$("#cs1").click(function(){
+		$("#cs1").css('background-color','white');
+		$("#cs1").css('color','black');
+		$("#cs").css('display','inline');
+
+		$("#bioe1").css('background-color','black');
+		$("#bioe1").css('color','white')
+		$("#bioe").css('display','none');
+		$("#ee1").css('background-color','black');
+		$("#ee1").css('color','white')
+		$("#ee").css('display','none');
+		$("#cee1").css('background-color','black');
+		$("#cee1").css('color','white')
+		$("#cee").css('display','none');
+		$("#ieor1").css('background-color','black');
+		$("#ieor1").css('color','white')
+		$("#ieor").css('display','none');
+		$("#mse1").css('background-color','black');
+		$("#mse1").css('color','white')
+		$("#mse").css('display','none');
+		$("#me1").css('background-color','black');
+		$("#me1").css('color','white')
+		$("#me").css('display','none');
+		$("#ne1").css('background-color','black');
+		$("#ne1").css('color','white')
+		$("#ne").css('display','none');
+	});
+	});
+	
+	$(document).ready(function(){
+		$("#ieor1").click(function(){
+		$("#ieor1").css('background-color','white');
+		$("#ieor1").css('color','black');
+		$("#ieor").css('display','inline');
+
+		$("#bioe1").css('background-color','black');
+		$("#bioe1").css('color','white')
+		$("#bioe").css('display','none');
+		$("#ee1").css('background-color','black');
+		$("#ee1").css('color','white')
+		$("#ee").css('display','none');
+		$("#cs1").css('background-color','black');
+		$("#cs1").css('color','white')
+		$("#cs").css('display','none');
+		$("#cee1").css('background-color','black');
+		$("#cee1").css('color','white')
+		$("#cee").css('display','none');
+		$("#mse1").css('background-color','black');
+		$("#mse1").css('color','white')
+		$("#mse").css('display','none');
+		$("#me1").css('background-color','black');
+		$("#me1").css('color','white')
+		$("#me").css('display','none');
+		$("#ne1").css('background-color','black');
+		$("#ne1").css('color','white')
+		$("#ne").css('display','none');
+	});
+	});
+	
+	$(document).ready(function(){
+		$("#mse1").click(function(){
+		$("#mse1").css('background-color','white');
+		$("#mse1").css('color','black');
+		$("#mse").css('display','inline');
+
+		$("#bioe1").css('background-color','black');
+		$("#bioe1").css('color','white')
+		$("#bioe").css('display','none');
+		$("#ee1").css('background-color','black');
+		$("#ee1").css('color','white')
+		$("#ee").css('display','none');
+		$("#cs1").css('background-color','black');
+		$("#cs1").css('color','white')
+		$("#cs").css('display','none');
+		$("#ieor1").css('background-color','black');
+		$("#ieor1").css('color','white')
+		$("#ieor").css('display','none');
+		$("#cee1").css('background-color','black');
+		$("#cee1").css('color','white')
+		$("#cee").css('display','none');
+		$("#me1").css('background-color','black');
+		$("#me1").css('color','white')
+		$("#me").css('display','none');
+		$("#ne1").css('background-color','black');
+		$("#ne1").css('color','white')
+		$("#ne").css('display','none');
+	});
+	});
+	
+	$(document).ready(function(){
+		$("#me1").click(function(){
+		$("#me1").css('background-color','white');
+		$("#me1").css('color','black');
+		$("#me").css('display','inline');
+
+		$("#bioe1").css('background-color','black');
+		$("#bioe1").css('color','white')
+		$("#bioe").css('display','none');
+		$("#ee1").css('background-color','black');
+		$("#ee1").css('color','white')
+		$("#ee").css('display','none');
+		$("#cs1").css('background-color','black');
+		$("#cs1").css('color','white')
+		$("#cs").css('display','none');
+		$("#ieor1").css('background-color','black');
+		$("#ieor1").css('color','white')
+		$("#ieor").css('display','none');
+		$("#mse1").css('background-color','black');
+		$("#mse1").css('color','white')
+		$("#mse").css('display','none');
+		$("#cee1").css('background-color','black');
+		$("#cee1").css('color','white')
+		$("#cee").css('display','none');
+		$("#ne1").css('background-color','black');
+		$("#ne1").css('color','white')
+		$("#ne").css('display','none');
+	});
+	});
+	
+	$(document).ready(function(){
+		$("#ne1").click(function(){
+		$("#ne1").css('background-color','white');
+		$("#ne1").css('color','black');
+		$("#ne").css('display','inline');
+
+		$("#bioe1").css('background-color','black');
+		$("#bioe1").css('color','white')
+		$("#bioe").css('display','none');
+		$("#ee1").css('background-color','black');
+		$("#ee1").css('color','white')
+		$("#ee").css('display','none');
+		$("#cs1").css('background-color','black');
+		$("#cs1").css('color','white')
+		$("#cs").css('display','none');
+		$("#ieor1").css('background-color','black');
+		$("#ieor1").css('color','white')
+		$("#ieor").css('display','none');
+		$("#mse1").css('background-color','black');
+		$("#mse1").css('color','white')
+		$("#mse").css('display','none');
+		$("#me1").css('background-color','black');
+		$("#me1").css('color','white')
+		$("#me").css('display','none');
+		$("#cee1").css('background-color','black');
+		$("#cee1").css('color','white')
+		$("#cee").css('display','none');
+	});
+	});
+	
+	// DOM Ready
+$(function() {
+
+    var $el, leftPos, newWidth;
+        $mainNav2 = $("#example-two");
+    
+    /*
+        EXAMPLE ONE
+    */
+    
+    /* Add Magic Line markup via JavaScript, because it ain't gonna work without */
+    $("#example-one").append("<li id='magic-line'></li>");
+    
+    /* Cache it */
+    var $magicLine = $("#magic-line");
+    
+    $magicLine
+        .width($(".current_page_item").width())
+        .css("left", $(".current_page_item a").position().left)
+        .data("origLeft", $magicLine.position().left)
+        .data("origWidth", $magicLine.width());
+        
+    $("#example-one li").find("a").hover(function() {
+        $el = $(this);
+        leftPos = $el.position().left;
+        newWidth = $el.parent().width();
+        
+        $magicLine.stop().animate({
+            left: leftPos,
+            width: newWidth
+        });
+    }, function() {
+        $magicLine.stop().animate({
+            left: $magicLine.data("origLeft"),
+            width: $magicLine.data("origWidth")
+        });    
+    });
+    
+    
+    
+    
+    /*
+        EXAMPLE TWO
+    */
+    
+    $mainNav2.append("<li id='magic-line-two'></li>");
+    
+    var $magicLineTwo = $("#magic-line-two");
+    
+    $magicLineTwo
+        .width($(".current_page_item_two").width())
+        .height($mainNav2.height())
+        .css("left", $(".current_page_item_two a").position().left)
+        .data("origLeft", $(".current_page_item_two a").position().left)
+        .data("origWidth", $magicLineTwo.width())
+        .data("origColor", $(".current_page_item_two a").attr("rel"));
+                
+    $("#example-two a").hover(function() {
+        $el = $(this);
+        leftPos = $el.position().left;
+        newWidth = $el.parent().width();
+        $magicLineTwo.stop().animate({
+            left: leftPos,
+            width: newWidth,
+            backgroundColor: $el.attr("rel")
+        })
+    }, function() {
+        $magicLineTwo.stop().animate({
+            left: $magicLineTwo.data("origLeft"),
+            width: $magicLineTwo.data("origWidth"),
+            backgroundColor: $magicLineTwo.data("origColor")
+        });    
+    });
+    
+    /* Kick IE into gear */
+    $(".current_page_item_two a").mouseenter();
+    
+});
